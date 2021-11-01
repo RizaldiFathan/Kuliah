9fa81729fd5f9dd96ceed252cdce63de84a273da
RizaldiFathan <brandalloka09@gmail.com>
2021-11-01 22:23:06 +0700

    update content

--- m6/kelompok_pmp.php
+++ m6/kelompok_pmp.php
@@ -1,3 +1,4 @@
+
 <!doctype html>
 <html lang="en">
   <head>
@@ -35,13 +36,13 @@
                         <input type="password" class="form-control" id="inputPassword3" onclick="Toggle()">
                       </div>
                     </div>
-                    <button class="btn btn-dark">Login</button>
+                    <button class="btn btn-dark mb-5">Login</button>
                 </form>
                 <table style="width:100%" class="mt-4 mb-4">
                     <thead>
                         <tr>
                             <th>Team</th>
-                            <th>Year</th>
+                            <th>Years</th>
                         </tr>
                     </thead>
                     <tbody>
@@ -71,23 +72,63 @@
                 </div>
             </div>
             <div class="col-md-9 p-5 card-right">
-                <div class="container">
-                    <div class="row">
-                        <div class="col-auto mx-auto d-block p-2">
-                            <img src="img/salah_1.jpg" alt="" height="179px">
+                <h1>Mohamed Salah</h1>
+                <div class="container mt-4">
+                    <div class="row mb-5">
+                        <div class="col-auto mx-auto d-block p-1">
+                            <a href="https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.lensaindonesia.com%2F2021%2F06%2F09%2Ffakta-menarik-tentang-mohamed-salah-sang-bintang-liverpool-asal-mesir.html&psig=AOvVaw0bVM5FyzFi8quRL2CHjNpI&ust=1635863372246000&source=images&cd=vfe&ved=0CAsQjRxqFwoTCMjpk6ew9_MCFQAAAAAdAAAAABAD">
+                            <img src="img/salah_1.jpg" alt="" width="282px" height="179px">
+                            </a>
                         </div>
-                        <div class="col-auto mx-auto d-block p-2">
-                            <img src="img/salah_2.jpg" alt="" height="179px">
+                        <div class="col-auto mx-auto d-block p-1">
+                            <a href="https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.independent.co.uk%2Fsport%2Ffootball%2Fatletico-madrid-liverpool-live-stream-champions-league-2021-b1941109.html%3Fpage%3D2&psig=AOvVaw3zmMg6aoJS4SrPuPWysmk6&ust=1635863973685000&source=images&cd=vfe&ved=0CAsQjRxqFwoTCPDju6qy9_MCFQAAAAAdAAAAABAD">
+                            <img src="img/salah_2.jpg" alt="" width="282px" height="179px">
+                        </a>
                         </div>
-                        <div class="col-auto mx-auto d-block p-2">
-                            <img src="img/salah_3.jpg" alt="" height="179px">
+                        <div class="col-auto mx-auto d-block p-1">
+                            <a href="https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.bola.net%2Finggris%2Fliverpool-targetkan-kontrak-baru-mohamed-salah-kelar-sebelum-2022-2df298.html&psig=AOvVaw0MN6NTIAxM4PxATwk-K5uz&ust=1635864025334000&source=images&cd=vfe&ved=0CAsQjRxqFwoTCNjo08yy9_MCFQAAAAAdAAAAABAD">
+                            <img src="img/salah_3.jpg" alt="" width="282px" height="179px">
+                        </a>
                         </div>
                     </div>
                 </div>
-                <p>Mohamed Salah Ghaly (bahasa Arab: محمد صلاح غالي‎ , pengucapan bahasa Arab Mesir: [mæˈħam.mæd sˤɑˈlɑːħ ˈɣæːli]; lahir 15 Juni 1992) adalah pemain sepak bola profesional Mesir yang bermain sebagai penyerang untuk klub Inggris Liverpool dan timnas Mesir.</p>
-                <p>Salah memulai karier seniornya di klub tempat lahirnya, El Mokawloon di Liga Premier Mesir tahun 2010, langsung bergabung dengan Basel untuk harga dirahasiakan. Di Swiss, ia menjadi bintang ketika ia memenangkan titel liga di musim debutnya, memenangkan SAFP Golden Player Award dalam proses. Performa Salah kemudian menarik minat klub Premier League Chelsea, dan ia bergabung dengan harga £11 juta pada tahun 2014. Namun, ia jarang digunakan di musim debutnya dan diizinkan pergi sebagai pinjaman ke klub Serie A Fiorentina dan Roma, dengan yang terakhir, membelinya secara permanen seharga €15 juta.</p>
-                <p>Menyusul performanya yang konsisten di Roma membawa mereka ke tempat kedua dan rekor poin total tahun 2017, Salah kembali ke Premier League untuk bergabung dengan Liverpool untuk rekor klub ketika itu, seharga £36.9 juta. Ketika masa keduanya di Inggris, Salah mengadaptasi permainannya dari sayap murni menjadi penyerang komplet dan dengan cepat menjadi titik fokus tim. Ia kemudian memecahkan rekor klub dalam musim debutnya, menerima Premier League Golden Boot setelah mencetak 32 gol dari 36 pertandingan. Ia juga menjadi pemain pertama yang memenangkan 3 Premier League Player of the Month dalam musim yang sama. Performanya yang luar biasa memberinya banyak penghargaan pada akhir musim 2017–18, termasuk PFA Players' Player of the Year, Football Writers' Player of the Year dan PFA Fans' Player of the Year. Salah meraih tempat ketiga untuk the 2018 Best FIFA Men's Player.</p>
-                <p>Dalam level internasional, Salah mewakili Mesir pada level muda, memenangkan medali perunggu dalam Africa U-20 Cup of Nations, dan berpartisipasi dalam 2011 FIFA U-20 World Cup dan 2012 Summer Olympics. Ia juga memenangkan CAF Most Promising African Talent Tahun 2012.[6] Membuat debutnya dengan timnas senior tahun 2011, ia membantu Mesir mencapai final 2017 Africa Cup of Nations, dan ia menjadi top skorer ketika kualifikasi CAF untuk membantu Mesir lolos ke 2018 FIFA World Cup. Untuk performanya, Salah dinamai CAF African Footballer of the Year dan BBC African Footballer of the Year.[7][8] Ia juga terpilih dalam CAF Team of the Year dan Africa Cup of Nations Team of the Tournament.</p>
+                <p>
+                    <b>Mohamed Salah Hamed Mahrous Ghaly</b> (<a href="https://en.wikipedia.org/wiki/Arabic_language" class="mw-redirect" title="Arabic language">Arabic</a>: <span lang="ar" dir="rtl">محمد صلاح حامد محروس غالي</span>‎, <small>Egyptian Arabic pronunciation:&nbsp;</small><span title="Representation in the International Phonetic Alphabet (IPA)" class="IPA"><a href="https://en.wikipedia.org/wiki/Help:IPA/Egyptian_Arabic" title="Help:IPA/Egyptian Arabic">[mæˈħam.mæd sˤɑˈlɑːħ ˈɣæːli]</a></span>; <i>born 15 June 1992 </i>) is an Egyptian professional <a href="https://en.wikipedia.org/wiki/Association_football" title="Association football">footballer</a> who plays as a <a href="https://en.wikipedia.org/wiki/Forward_(association_football)" title="Forward (association football)">forward</a> for <a href="https://en.wikipedia.org/wiki/Premier_League" title="Premier League">Premier League</a> club <a href="https://en.wikipedia.org/wiki/Liverpool_F.C." title="Liverpool F.C.">Liverpool</a> and <a href="https://en.wikipedia.org/wiki/Captain_(association_football)" title="Captain (association football)">captains</a> the <a href="https://en.wikipedia.org/wiki/Egypt_national_football_team" title="Egypt national football team">Egypt national team</a>. Considered one of the best players in the world, he is known for his finishing, dribbling, and speed.
+                </p>
+                <p>
+                    Salah started his senior career with Egyptian club <a href="https://en.wikipedia.org/wiki/Al_Mokawloon_Al_Arab_SC" title="Al Mokawloon Al Arab SC">Al Mokawloon</a>, departing shortly thereafter to join Swiss side <a href="https://en.wikipedia.org/wiki/Al_Mokawloon_Al_Arab_SC" title="FC Basel">Basel</a> for an undisclosed fee. In Switzerland, his performances attracted Premier League side <a href="https://en.wikipedia.org/wiki/Chelsea_F.C." title="Chelsea F.C.">Chelsea</a>, who signed him in 2014 for a reported fee of £11&nbsp;million. However, he was used sparingly in his debut season and was allowed to leave on loan to <a href="https://en.wikipedia.org/wiki/Serie_A" title="Serie A">Serie A</a> clubs <a href="https://en.wikipedia.org/wiki/ACF_Fiorentina" title="ACF Fiorentina">Fiorentina</a> and <a href="https://en.wikipedia.org/wiki/A.S._Roma" title="A.S. Roma">Roma</a>, with the latter eventually signing him permanently for €15&nbsp;million. Following his role in helping Roma to a <a href="https://en.wikipedia.org/wiki/2016%E2%80%9317_A.S._Roma_season" title="2016–17 A.S. Roma season">second-placed finish and a club record points tally</a> in 2017, Salah returned to the Premier League to sign for Liverpool for a then club-record fee of £36.9&nbsp;million.
+                </p>
+                <p>
+                    In his second spell in England, Salah adapted his game from a natural winger to a forward, and he netted 32 goals in 36 league games in his debut season, winning the <a href="https://en.wikipedia.org/wiki/Premier_League_Golden_Boot" title="Premier League Golden Boot">Premier League Golden Boot</a> and breaking the Premier League scoring record for a 38-game season. Salah's performances earned him a number of individual accolades, including the <a href="https://en.wikipedia.org/wiki/PFA_Players%27_Player_of_the_Year" title="PFA Players' Player of the Year">PFA Players' Player of the Year</a>. He also came third in the <a href="https://en.wikipedia.org/wiki/The_Best_FIFA_Men%27s_Player#Winners" title="The Best FIFA Men's Player">2018 Best FIFA Men's Player</a> award. The <a href="https://en.wikipedia.org/wiki/2018%E2%80%9319_Liverpool_F.C._season" title="2018–19 Liverpool F.C. season">following season</a>, he finished as the Premier League's joint-top goalscorer and helped Liverpool win the <a href="https://en.wikipedia.org/wiki/2018%E2%80%9319_UEFA_Champions_League" title="2018–19 UEFA Champions League">2018–19 UEFA Champions League</a>, scoring in the final. In his third season at the club, he helped the team win the <a href="" title="2019–20 Premier League">2019–20 Premier League</a>, which ended the club's 30-year league title drought.
+                </p>
+                <p>
+                    At international level, Salah represented Egypt at youth level before making his senior debut in 2011. Following his performances at the <a href="https://en.wikipedia.org/wiki/2012_Summer_Olympics" title="2012 Summer Olympics">2012 Summer Olympics</a>, he was named <a href="https://en.wikipedia.org/wiki/CAF_Awards" title="CAF Awards">CAF Most Promising African Talent</a> of the Year. Since then, he has helped Egypt reach the final of the <a href="https://en.wikipedia.org/wiki/2017_Africa_Cup_of_Nations" title="2017 Africa Cup of Nations">2017 Africa Cup of Nations</a>, and was top scorer during <a href="https://en.wikipedia.org/wiki/2018_FIFA_World_Cup_qualification_(CAF)" title="2018 FIFA World Cup qualification (CAF)">CAF qualification</a> to help the team qualify for the <a href="https://en.wikipedia.org/wiki/2018_FIFA_World_Cup" title="2018 FIFA World Cup">2018 FIFA World Cup</a>. Salah has been named the <a href="https://en.wikipedia.org/wiki/African_Footballer_of_the_Year" title="African Footballer of the Year">CAF African Footballer of the Year</a> (2017 and 2018), <a href="https://en.wikipedia.org/wiki/BBC_African_Footballer_of_the_Year" title="BBC African Footballer of the Year">BBC African Footballer of the Year</a> (2017 and 2018), and was selected in the <a href="https://en.wikipedia.org/wiki/BBC_African_Footballer_of_the_Year" title="2017 Africa Cup of Nations">2017 Africa Cup of Nations Team of the Tournament</a> and the <a href="https://en.wikipedia.org/wiki/CAF_Team_of_the_Year" title="CAF Team of the Year">CAF Team of the Year</a> on several occasions.
+                </p>
+                <div class="container-fluid">
+                    <div class="row">
+                        <div class="col-auto mx-auto d-block p-2">
+                            <a href="https://en.wikipedia.org/wiki/File:Zenit-bazel_2013_(2).jpg" class="image"><img alt="" src="//upload.wikimedia.org/wikipedia/commons/thumb/4/47/Zenit-bazel_2013_%282%29.jpg/170px-Zenit-bazel_2013_%282%29.jpg" decoding="async" width="170" height="255" class="thumbimage" srcset="//upload.wikimedia.org/wikipedia/commons/thumb/4/47/Zenit-bazel_2013_%282%29.jpg/255px-Zenit-bazel_2013_%282%29.jpg 1.5x, //upload.wikimedia.org/wikipedia/commons/4/47/Zenit-bazel_2013_%282%29.jpg 2x" data-file-width="283" data-file-height="494"></a>
+                        </div>
+                        <div class="col-auto mx-auto d-block p-2">
+                            <a href="https://en.wikipedia.org/wiki/File:Mohamed_Salah_with_Chelsea_-_Jan_2015.jpg" class="image"><img alt="" src="//upload.wikimedia.org/wikipedia/commons/thumb/e/ed/Mohamed_Salah_with_Chelsea_-_Jan_2015.jpg/150px-Mohamed_Salah_with_Chelsea_-_Jan_2015.jpg" decoding="async" width="170" height="255" class="thumbimage" srcset="//upload.wikimedia.org/wikipedia/commons/thumb/e/ed/Mohamed_Salah_with_Chelsea_-_Jan_2015.jpg/225px-Mohamed_Salah_with_Chelsea_-_Jan_2015.jpg 1.5x, //upload.wikimedia.org/wikipedia/commons/thumb/e/ed/Mohamed_Salah_with_Chelsea_-_Jan_2015.jpg/300px-Mohamed_Salah_with_Chelsea_-_Jan_2015.jpg 2x" data-file-width="2414" data-file-height="3620"></a>
+                        </div>
+                        <div class="col-auto mx-auto d-block p-2">
+                            <a href="https://en.wikipedia.org/wiki/File:Mohamed_Salah_2015.jpg" class="image"><img alt="" src="//upload.wikimedia.org/wikipedia/commons/thumb/3/3e/Mohamed_Salah_2015.jpg/170px-Mohamed_Salah_2015.jpg" decoding="async" width="170" height="255" class="thumbimage" srcset="//upload.wikimedia.org/wikipedia/commons/thumb/3/3e/Mohamed_Salah_2015.jpg/255px-Mohamed_Salah_2015.jpg 1.5x, //upload.wikimedia.org/wikipedia/commons/thumb/3/3e/Mohamed_Salah_2015.jpg/340px-Mohamed_Salah_2015.jpg 2x" data-file-width="597" data-file-height="900"></a>
+                        </div>
+                        <div class="col-auto mx-auto d-block p-2">
+                            <a href="https://en.wikipedia.org/wiki/File:Mohamed_Salah_2017.jpg" class="image"><img alt="" src="//upload.wikimedia.org/wikipedia/commons/thumb/a/af/Mohamed_Salah_2017.jpg/170px-Mohamed_Salah_2017.jpg" decoding="async" width="170" height="255" class="thumbimage" srcset="//upload.wikimedia.org/wikipedia/commons/thumb/a/af/Mohamed_Salah_2017.jpg/255px-Mohamed_Salah_2017.jpg 1.5x, //upload.wikimedia.org/wikipedia/commons/thumb/a/af/Mohamed_Salah_2017.jpg/340px-Mohamed_Salah_2017.jpg 2x" data-file-width="551" data-file-height="826"></a>
+                        </div>
+                    </div>
+                </div>
+                
+
+                
+
+                
+
+               
+
+                
             </div>
         </div>
     </div>
